b29aeb21a7094a603cec43ca8e13c4dc0f92d107
Monjy HaingoArilala <monjy.randrianjatovo@arctic-lab.fr>
2021-08-11 15:12:12 +0300

    add index and 404 pages

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const db = require('./app/models')
 const app = express()
 
@@ -12,9 +13,15 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'app/views'))
 
 app.get('/', (req, res) => {
-  res.json({ message: '' })
+  res.render('index')
+})
+
+app.get('*', function (req, res) {
+  res.render('not_found')
 })
 
 require('./app/routes/user.routes')(app)
